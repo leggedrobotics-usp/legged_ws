8d15399b0bd667e3847d24091ae74a26d002739e
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2022-09-27 17:42:05 -0300

    [ADDED] automatic container start for docker_attach.

--- docker_attach.sh
+++ docker_attach.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 CONTAINER_LABEL=ros_$1
 
+CONTAINER_EXIST=0
+
+if [ -z $(docker container ls --format="{{.Names}}" | grep $CONTAINER_LABEL) ]; then
+    ./docker_run_detached.sh $1
+fi
+
+docker start $CONTAINER_LABEL
 docker exec -it $CONTAINER_LABEL "/bin/bash"
