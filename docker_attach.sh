4cfec63cb5b10bac24c7ab909e2349fa46df46e4
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2023-01-23 13:47:51 -0300

    [FIXED] docker_attach.sh for inactive containers with the same container label.

--- docker_attach.sh
+++ docker_attach.sh
@@ -3,7 +3,7 @@ CONTAINER_LABEL=ros_$1
 
 CONTAINER_EXIST=0
 
-if [ -z $(docker container ls --format="{{.Names}}" | grep $CONTAINER_LABEL) ]; then
+if [ -z $(docker container ls -a --format="{{.Names}}" | grep $CONTAINER_LABEL) ]; then
     ./docker_run_detached.sh $1
 fi
 
