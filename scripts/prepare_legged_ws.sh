f8b8184a89c62fd1a1c3db838d760db9cfc3d68e
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2023-01-24 16:54:11 -0300

    [ADDED] prepare_legged_ws.sh for multiple images. Preferable noetic-legged.

---
+++ scripts/prepare_legged_ws.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Please, give me a ROS name. Check dockerfiles in docker folder."
+
+else
+    CONTAINER_HOME=docker/container/$1/home/$USER
+    WS_SRC_FOLDER=$CONTAINER_HOME/catkin_ws/src
+    SPLITED_ONE=($(echo $1 | tr "-" "\n"))
+
+    echo "source /opt/ros/${SPLITED_ONE[0]}/setup.bash" >> $CONTAINER_HOME/.bashrc
+
+    mkdir -p $WS_SRC_FOLDER
+
+    echo "This may take a while... Downloading needed packages' repositories..."
+
+    git clone -b master git@github.com:lomcin/legged_control.git $WS_SRC_FOLDER/legged_control
+    # Clone OCS2
+    git clone https://github.com/leggedrobotics/ocs2.git $WS_SRC_FOLDER/ocs2
+    # Clone pinocchio
+    git clone --recurse-submodules https://github.com/leggedrobotics/pinocchio.git $WS_SRC_FOLDER/pinocchio
+    # Clone hpp-fcl
+    git clone --recurse-submodules https://github.com/leggedrobotics/hpp-fcl.git $WS_SRC_FOLDER/hpp-fcl
+    # Clone ocs2_robotic_assets
+    git clone https://github.com/leggedrobotics/ocs2_robotic_assets.git $WS_SRC_FOLDER/ocs2_robotic_assets
+
+    # catkin config -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    # catkin build ocs2_legged_robot_ros
+
+    echo "Building $1 docker image..."
+
+    ./docker_build.sh $1
+
+fi
