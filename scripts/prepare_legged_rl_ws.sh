ae98631f666d28fe3cae165a853ef535f02df72b
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2023-03-27 11:42:24 -0300

    [FIXED] go1_rl default branch in prepare_legged_rl_ws.sh.

---
+++ scripts/prepare_legged_rl_ws.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Please, give me a ROS name. Check dockerfiles in docker folder."
+
+else
+    if [ -z $2 ]; then
+        USER_NAME=catkin
+        CONTAINER_ALIAS=$1
+    else
+        USER_NAME=$2
+        CONTAINER_ALIAS=$1-$2
+    fi
+
+    CONTAINER_HOME=docker/container/$CONTAINER_ALIAS/home/$USER
+    WS_SRC_FOLDER=$CONTAINER_HOME/catkin_ws/src
+    SPLITED_ONE=($(echo $1 | tr "-" "\n"))
+
+    echo "source /opt/ros/${SPLITED_ONE[0]}/setup.bash" >> $CONTAINER_HOME/.bashrc
+
+    mkdir -p $WS_SRC_FOLDER
+
+    echo "This may take a while... Downloading needed packages' repositories..."
+
+    # Clone go1_rl
+    git clone -b main git@github.com:lomcin/go1_rl.git $WS_SRC_FOLDER/go1_rl
+
+    ./scripts/prepare_legged_ws.sh ${@}
+
+fi
