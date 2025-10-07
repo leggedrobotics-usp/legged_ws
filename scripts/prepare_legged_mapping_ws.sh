449e95abd82b302cb6d5ac87ac0bf515cb5a574e
toschilt <lt.toschi@gmail.com>
2025-10-07 14:18:31 -0300

    fix: change hhtps URLs to ssh

--- scripts/prepare_legged_mapping_ws.sh
+++ scripts/prepare_legged_mapping_ws.sh
@@ -24,7 +24,7 @@ else
     echo "This may take a while... Downloading needed packages' repositories..."
 
     # Clone legged_control
-    git clone -b navigation_test_am https://github.com/leggedrobotics-usp/legged_control.git $WS_SRC_FOLDER/legged_control
+    git clone -b navigation_test_am git@github.com:leggedrobotics-usp/legged_control.git $WS_SRC_FOLDER/legged_control
     # Clone OCS2
     git clone https://github.com/leggedrobotics/ocs2.git $WS_SRC_FOLDER/ocs2
     # Clone pinocchio
@@ -62,17 +62,14 @@ else
     # Clone rtabmap_ros
     git clone -b noetic-devel https://github.com/introlab/rtabmap_ros.git $WS_SRC_FOLDER/rtabmap_ros
 
-
-
     # Clone mapping legro    
-    git clone -b am2/dev https://github.com/leggedrobotics-usp/mapping_legro.git $WS_SRC_FOLDER/mapping_legro
+    git clone -b am2/dev git@github.com:leggedrobotics-usp/mapping_legro.git $WS_SRC_FOLDER/mapping_legro
 
     # Clone am navigation   
-    git clone -b am2/dev https://github.com/leggedrobotics-usp/am_navigation.git $WS_SRC_FOLDER/am_navigation
+    git clone -b am2/dev git@github.com:leggedrobotics-usp/am_navigation.git $WS_SRC_FOLDER/am_navigation
 
     # Colocando o am_maps
-    git clone -b am/dev https://github.com/leggedrobotics-usp/am_maps.git $WS_SRC_FOLDER/am_maps
-
+    git clone -b am/dev git@github.com:leggedrobotics-usp/am_maps.git $WS_SRC_FOLDER/am_maps
 
     git clone https://github.com/Livox-SDK/livox_ros_driver2.git $WS_SRC_FOLDER/livox_ros_driver2
 
@@ -80,7 +77,7 @@ else
 
     git clone https://github.com/koide3/livox_to_pointcloud2.git $WS_SRC_FOLDER/livox_to_pointcloud2
 
-    git clone https://github.com/leggedrobotics-usp/fiducial_detect_ros $WS_SRC_FOLDER/fiducial_detect_ros
+    git clone git@github.com:leggedrobotics-usp/fiducial_detect_ros.git $WS_SRC_FOLDER/fiducial_detect_ros
 
     git clone https://github.com/hku-mars/FAST_LIO $WS_SRC_FOLDER/FAST_LIO
     cd $WS_SRC_FOLDER/FAST_LIO
